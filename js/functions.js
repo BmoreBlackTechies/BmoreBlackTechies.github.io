86c5872d0dd11b8bcfb0e5adbf8fc366bde72cd0
Dominique Cummings <dcummin07@gmail.com>
2018-06-06 19:06:35 -0400

    Updated navigation menu layout

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,7 @@
 $("#filter-button").click(function(){
   $("#toggle-filter").slideToggle();
-});
+}); // slide down toggle for sorting options
+
+$(".navbar-icon").click(function(){
+  $(".navlinks").slideToggle();
+}); // slide down toggle for navigation
