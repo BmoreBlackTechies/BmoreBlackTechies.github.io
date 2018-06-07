a103871f880af20b83eae36cf8185e786910ec9b
Dominique Cummings <dcummin07@gmail.com>
2018-06-06 20:25:27 -0400

    Added search field

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,11 @@ $("#filter-button").click(function(){
 $(".navbar-icon").click(function(){
   $(".navlinks").slideToggle();
 }); // slide down toggle for navigation
+
+$(".search-input").click(function(){
+  $(".fa-search").css("color", "blue");
+}); // search icon turns blue when you click on input field
+
+$(".search-input").focusout(function(){
+  $(".fa-search").css("color", "grey");
+}); // search icon turns grey when you click outside of input field
